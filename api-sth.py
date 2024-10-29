96308ed2671d625920b0e25e0d408e3a6339cd7d
Lucas Oliveira Silva <127321390+Lucas-O-S@users.noreply.github.com>
2024-10-29 12:13:30 -0300

    Gráfico de temperatura adicionado

--- api-sth.py
+++ api-sth.py
@@ -12,9 +12,9 @@ PORT_STH = 8666
 DASH_HOST = "0.0.0.0"  # Set this to "0.0.0.0" to allow access from any IP
 lamp = "05x"
  
-# Function to get luminosity data from the API
-def get_luminosity_data(lastN):
-    url = f"http://{IP_ADDRESS}:{PORT_STH}/STH/v1/contextEntities/type/Lamp/id/urn:ngsi-ld:Lamp:{lamp}/attributes/luminosity?lastN={lastN}"
+# Function to get data from the API
+def get_data(lastN,dataType):
+    url = f"http://{IP_ADDRESS}:{PORT_STH}/STH/v1/contextEntities/type/Lamp/id/urn:ngsi-ld:Lamp:{lamp}/attributes/{dataType}?lastN={lastN}"
     headers = {
         'fiware-service': 'smart',
         'fiware-servicepath': '/'
@@ -31,8 +31,9 @@ def get_luminosity_data(lastN):
     else:
         print(f"Error accessing {url}: {response.status_code}")
         return []
+
  
-# Function to convert UTC timestamps to Lisbon time
+# Function to convert UTC timestamps to São Paulo time
 def convert_to_sao_paulo_time(timestamps):
     utc = pytz.utc
     lisbon = pytz.timezone('America/Sao_Paulo')
@@ -53,15 +54,25 @@ lastN = 10  # Get 10 most recent points at each interval
 app = dash.Dash(__name__)
  
 app.layout = html.Div([
-    html.H1('Luminosity Data Viewer'),
+    html.H1('ESP 32 Data Viewer'),
+    dcc.Graph(id='general-graph'),
+
     dcc.Graph(id='luminosity-graph'),
+    dcc.Graph(id='temperature-graph'),
+    dcc.Graph(id='humidity-graph'),
+
     # Store to hold historical data
     dcc.Store(id='luminosity-data-store', data={'timestamps': [], 'luminosity_values': []}),
+    dcc.Store(id='temperature-data-store', data={'timestamps': [], 'temperature_values': []}),
+    dcc.Store(id='humidity-data-store', data={'timestamps': [], 'humidity': []}),
+
     dcc.Interval(
         id='interval-component',
         interval=10*1000,  # in milliseconds (10 seconds)
         n_intervals=0
     )
+    
+    
 ])
 
 @app.callback(
@@ -71,7 +82,7 @@ app.layout = html.Div([
 )
 def update_data_store(n, stored_data):
     # Get luminosity data
-    data_luminosity = get_luminosity_data(lastN)
+    data_luminosity = get_data(lastN,"luminosity")
 
     if data_luminosity:
         # Extract values and timestamps
@@ -134,6 +145,80 @@ def update_graph(stored_data):
         )
 
         return fig_luminosity
+
+    return {}
+
+
+@app.callback(
+    Output('temperature-data-store', 'data'),
+    Input('interval-component', 'n_intervals'),
+    State('temperature-data-store', 'data')
+)
+def update_data_store(n, stored_data):
+    # Get luminosity data
+    data_temperature = get_data(lastN,"temperature")
+
+    if data_temperature:
+        # Extract values and timestamps
+        temperature_values = [float(entry['attrValue']) for entry in data_temperature]  # Ensure values are floats
+        timestamps = [entry['recvTime'] for entry in data_temperature]
+
+        # Calculate the average luminosity for the current interval
+        average_temperature = sum(temperature_values) / len(temperature_values)
+
+        # Convert timestamps to Lisbon time
+        timestamps = convert_to_sao_paulo_time(timestamps)
+
+        # Append the new average and the latest timestamp to stored data
+        stored_data['timestamps'].append(timestamps[-1])  # Store only the latest timestamp
+        stored_data['temperature_values'].append(average_temperature)  # Store the average luminosity
+
+        # Calculate total average luminosity
+        total_temperature = sum(stored_data['temperature_values'])
+        total_count = len(stored_data['temperature_values'])
+        stored_data['total_average_temperature'] = total_temperature / total_count if total_count > 0 else 0
+
+        return stored_data
+
+    return stored_data
+
+@app.callback(
+    Output('temperature-graph', 'figure'),
+    Input('temperature-data-store', 'data')
+)
+def update_graph(stored_data):
+    if stored_data['timestamps'] and stored_data['temperature_values']:
+        # Create traces for the plot
+        trace_average = go.Scatter(
+            x=stored_data['timestamps'],
+            y=stored_data['temperature_values'],
+            mode='lines+markers',
+            name='Temperatura',
+            line=dict(color='red')
+        )
+
+        # Create a trace for the total average
+        total_average_temperature = stored_data['total_average_temperature']
+        trace_total_average = go.Scatter(
+            x=[stored_data['timestamps'][0], stored_data['timestamps'][-1]],
+            y=[total_average_temperature, total_average_temperature],
+            mode='lines',
+            name='Media da Temperatura',
+            line=dict(color='blue', dash='dash')
+        )
+
+        # Create figure
+        fig_temperature = go.Figure(data=[trace_average, trace_total_average])
+
+        # Update layout
+        fig_temperature.update_layout(
+            title='Temperatura',
+            xaxis_title='Timestamp',
+            yaxis_title='temperature',
+            hovermode='closest'
+        )
+
+        return fig_temperature
 
     return {}
 
